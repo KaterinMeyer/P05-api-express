6e491edcc55de3902349912ecef4dd28a833f339
KaterinMeyer <katerinmeyer@gmail.com>
2023-07-16 12:21:20 -0400

    se arreglan problemas para render

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import express from 'express'
 
-const app  = express()
+const app = express()
 
 app.use(express.json())
 
 
-app.use ('/', function (req,res){
-    return res.status(200).json({msg: "Bienvenido"})
+app.use('/', function (req, res) {
+    return res.status(200).json({ msg: "Bienvenido" })
 })
 
-app.listen(3000,() => {
+app.listen(3000, () => {
     console.log("Aplicación iniciada en puerto 3000")
 })
