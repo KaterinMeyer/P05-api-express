6ce74dac19ec7702de36bbb6a908c77454eacb42
Katerin Meyer <katerinmeyer@gmail.com>
2023-07-19 21:36:43 -0400

    Mongoose

---
+++ src/resources/users/routes/users.routes.js
@@ -0,0 +1,13 @@
+import {Router} from 'express'
+import {createUser, deleteUserById, getUserById, getUsers, updateUserById} from '../controllers/users.controllers.js'
+
+const usersRouters = Router()
+const baseURI = '/users'
+
+usersRouters.post(baseURI, createUser)
+usersRouters.get (baseURI, getUsers)
+usersRouters.get (`${baseURI}/:id`, getUserById)
+usersRouters.patch (`${baseURI}/:id`, updateUserById)
+usersRouters.delete (`${baseURI}/:id`, deleteUserById)
+
+export default usersRouterss
