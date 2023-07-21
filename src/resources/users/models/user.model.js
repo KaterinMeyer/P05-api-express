0183fa9bb796ac80b3d0ee65b35a8c906f287be9
Katerin Meyer <katerinmeyer@gmail.com>
2023-07-21 17:54:22 -0400

    Se agregan Middlewares

--- src/resources/users/models/user.model.js
+++ src/resources/users/models/user.model.js
@@ -13,8 +13,21 @@ const userSchema = new mongoose.Schema({
         type: Number,
         required: false,
     },
-}, {versionKey: false})
+}, { versionKey: false })
 // Si se dejara en true, al momento de crear un elemento le va a crear una versión
+
+userSchema.pre('save', function (next) {
+    // Se ejecuta antes del save y permite modificar el documento que está siendo guardado
+    console.log("Usuario a agregar")
+    console.log(this.jSON())
+    next()
+})
+
+userSchema.post('save', function(document) {
+    console.log("Usuario agregado")
+    console.log( document )
+
+})
 
 export const UserModel = new mongoose.model('User', userSchema)
 
