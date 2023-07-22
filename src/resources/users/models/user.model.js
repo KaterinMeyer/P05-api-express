93074b774ffcf2887f1e98690b2520c14349d499
Katerin Meyer <katerinmeyer@gmail.com>
2023-07-22 13:28:48 -0400

    Se agrega libreria para validar y acutalización esquema mongoose

--- src/resources/users/models/user.model.js
+++ src/resources/users/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import validator from 'validator'
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -9,10 +10,44 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    email: {
+        type: String,
+        lowercase: true,
+        validate: function (email) {
+            return validator.isEmail(email)
+        }
+    },
     age: {
         type: Number,
         required: false,
+        default: 0
     },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    pets: [
+        {
+            type: String
+        }
+    ],
+    addresses: [
+        {
+            street: {
+                type: String,
+                required: true
+            },
+            region: {
+                type: String,
+                required: true
+            },
+            number: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 }, { versionKey: false, toJSON: { virtuals: true }, toObject: { virtuals: true } })
 // Si se dejara en true, al momento de crear un elemento le va a crear una versión
 
