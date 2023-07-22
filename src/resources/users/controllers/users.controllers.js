633c0e0ec361ee3b7bce3c284ee73de8f3c8efba
Katerin Meyer <katerinmeyer@gmail.com>
2023-07-22 13:45:11 -0400

    Se agregan filtro, isadmin, en user controller

--- src/resources/users/controllers/users.controllers.js
+++ src/resources/users/controllers/users.controllers.js
@@ -13,12 +13,29 @@ export async function createUser(req, res) {
 
 
 export async function getUsers(req, res) {
-    const [users, error] = await awaitCatcher(UserModel.find())
+    const queryParams = req.query
+    const queryObj = {}
+    const queryKeys = Object.keys(queryParams)
+    if (queryKeys.length > 0) {
+        for (const key of queryKeys) {
+            if (key === 'age') {
+                queryObj[key] = Number(queryParams[key])
+                continue
+            }
+            if (key === "isAdmin") {
+                queryObj[key] = queryParams[key] === 'true'
+            }
+            console.log(key);
+        }
+    }
+    console.log(queryParams)
+    console.log(queryObj)
+    const [users, error] = await awaitCatcher(UserModel.find(queryObj, { pets: 0, addresses: 0 }))
     if (error) {
         return res.status(400).json({ status: "error", msg: error.message })
     }
-    return res.status(201).json(userCreated)
-} 
+    return res.status(201).json(users)
+}
 
 export async function getUserById(req, res) {
     const id = req.params.id
@@ -32,12 +49,12 @@ export async function getUserById(req, res) {
 export async function updateUserById(req, res) {
     const id = req.params.id
     const body = req.body
-    const [userUpdated, error] = await awaitCatcher(UserModel.findByIdAndUpdate(id , body, {new: true}))
+    const [userUpdated, error] = await awaitCatcher(UserModel.findByIdAndUpdate(id, body, { new: true }))
     if (error) {
         return res.status(404).json({ status: "error", msg: "usuario no encontrado" })
     }
     return res.status(200).json(userUpdated)
-}  
+}
 
 export async function deleteUserById(req, res) {
     const id = req.params.id
